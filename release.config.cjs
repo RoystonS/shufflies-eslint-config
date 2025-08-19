057930496985b877b2234af8902ca955a6ade381
Royston Shufflebotham <royston@shufflebotham.org>
2025-08-19 23:10:54 +0100

    chore(build): perform a minor release on eslint deps bump

--- release.config.cjs
+++ release.config.cjs
@@ -2,9 +2,21 @@ module.exports = {
   branches: ["main"],
   plugins: [
     // Analyze commit messages to determine the type of release
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          // Default: ignore all plain chore(deps)
+          { type: "chore", scope: "deps", release: false },
+          // Special-case: treat ESLint bumps as a minor release
+          { type: "chore", scope: "deps", release: "minor", subject: /bump.*(eslint)/i },
+        ]
+      }
+    ],
+
     // Generate release notes based on commit messages
     "@semantic-release/release-notes-generator",
+
     // Publish the package to npm
     "@semantic-release/npm",
     // Create a GitHub release
